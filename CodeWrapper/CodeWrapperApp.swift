619c9163f0c8c78db4e46ddf74c6baa08ff9c6fb
Navtoj Chahal <github@navtoj.com>
2024-09-22 01:09:49 -0700

    Create Xcode Project
    macOS SwiftUI App Template

---
+++ CodeWrapper/CodeWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWrapperApp.swift
+//  CodeWrapper
+//
+//  Created by Navtoj Chahal on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct CodeWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
