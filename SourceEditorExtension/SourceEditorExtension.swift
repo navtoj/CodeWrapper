8f2d815e2ea3b6f57023849e42542153bb85c45d
Navtoj Chahal <github@navtoj.com>
2024-09-22 02:48:41 -0700

    Add Helper Function

--- SourceEditorExtension/SourceEditorExtension.swift
+++ SourceEditorExtension/SourceEditorExtension.swift
@@ -9,18 +9,37 @@ import Foundation
 import XcodeKit
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
-    
-    /*
-    func extensionDidFinishLaunching() {
-        // If your extension needs to do any work at launch, implement this optional method.
-    }
-    */
-    
-    /*
-    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
-        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-        return []
-    }
-    */
-    
+
+	// If your extension needs to do any work at launch, implement this optional method.
+
+	/*
+	func extensionDidFinishLaunching() {}
+	*/
+
+	// If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+
+	/*
+	var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+		guard let bundleId = Bundle(for: type(of: self)).bundleIdentifier else { return [] }
+		return [
+			makeDefinition(bundleId, DebugCommand.className(), "#if DEBUG")
+		]
+	}
+	*/
 }
+
+// Helper Function
+
+/*
+private func makeDefinition(
+	_ bundleId: String,
+	_ className: String,
+	_ commandName: String
+) -> [XCSourceEditorCommandDefinitionKey: Any] {
+	[
+		XCSourceEditorCommandDefinitionKey.identifierKey: bundleId + "." + className,
+		XCSourceEditorCommandDefinitionKey.classNameKey: className,
+		XCSourceEditorCommandDefinitionKey.nameKey: commandName
+	]
+}
+*/
