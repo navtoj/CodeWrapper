a44ccfcce41903b28305ec367e76481186769338
Navtoj Chahal <github@navtoj.com>
2024-09-22 02:11:36 -0700

    Update Display Names
    https://nshipster.com/xcode-source-extensions/#display-names

--- SourceEditorExtension/DebugCommand.swift
+++ SourceEditorExtension/DebugCommand.swift
@@ -1,5 +1,5 @@
 //
-//  SourceEditorCommand.swift
+//  DebugCommand.swift
 //  SourceEditorExtension
 //
 //  Created by Navtoj Chahal on 2024-09-22.
@@ -8,7 +8,7 @@
 import Foundation
 import XcodeKit
 
-class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+class DebugCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
