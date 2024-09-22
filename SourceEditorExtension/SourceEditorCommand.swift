7197830fc7a288e132bcfcfa782c46a2b0030a50
Navtoj Chahal <github@navtoj.com>
2024-09-22 01:12:27 -0700

    Create Xcode Source Editor Extension

---
+++ SourceEditorExtension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  SourceEditorExtension
+//
+//  Created by Navtoj Chahal on 2024-09-22.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
