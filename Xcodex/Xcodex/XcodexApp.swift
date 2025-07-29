77e9cc88a0a3eea11990375b801351942e7bd320
Navtoj Chahal <github@navtoj.com>
2025-07-28 19:18:52 -0700

    Create macOS App

---
+++ Xcodex/Xcodex/XcodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodexApp.swift
+//  Xcodex
+//
+//  Created by Navtoj Chahal on 2025-07-28.
+//
+
+import SwiftUI
+
+@main
+struct XcodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
